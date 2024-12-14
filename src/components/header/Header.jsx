1358e5bc5097b4d4d18c8486ff17ccaa4833ab01
cel004 <ngcelinaa@gmail.com>
2024-12-14 02:45:17 -0500

    Contain navigation bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,11 @@ function Header() {
       <>
       <div className="headerBar">
         <p className={styles.title}>Pixcel</p>
-        <div className="navigationBar">
-            <a href="#">About</a>
-            <a href="#">Pricing</a>
-            <a href="#">Contact</a>
-        </div>
+            <div className="navigationBar">
+                <a href="#">About</a>
+                <a href="#">Pricing</a>
+                <a href="#">Contact</a>
+            </div>
       </div>
       </>
     )
