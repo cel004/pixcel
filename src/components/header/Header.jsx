d90dfb0f68a51df815c02585fd002c420d982b2e
cel004 <ngcelinaa@gmail.com>
2024-12-15 01:04:01 -0500

    Layout description texts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,31 @@
 import styles from './Header.module.css'
+import logo from '/pixcel_logo.png'
 
 function Header() {
     return (
       <>
-      <div className="headerBar">
-        <p className={styles.title}>Pixcel</p>
-            <div className="navigationBar">
-                <a href="#">About</a>
-                <a href="#">Pricing</a>
-                <a href="#">Contact</a>
+      <div className={styles.headerBar}>
+        <div className={styles.title}>Pixcel</div>
+          <div className={styles.right}>
+            <div className={styles.navigationBar}>
+              <a href="#">About</a>
+              <a href="#">Pricing</a>
+              <a href="#">Contact</a>
             </div>
+            <img src={logo} alt="Pixcel's logo" />
+          </div>
       </div>
+      <div className={styles.textContainer}>
+        <p style={{ fontWeight: '500', fontSize: '1.5rem'}}>Hand-coded with care,</p>
+        <p>our websites are crafted with empathy to redefine 
+          your digital presence, focusing on what truly matters to you.</p>
+      </div>
+      <div className={styles.detailsContainer}>
+        <p>100% Handmade</p>
+        <p>Fully Responsive</p>
+        <p>Optimized Page Speed</p>
+        <p>SEO</p>
+    </div>
       </>
     )
   }
