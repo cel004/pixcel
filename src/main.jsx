a52aa6a1c4dbeed15e8fb9b8ec7947482fc6156b
cel004 <ngcelinaa@gmail.com>
2024-12-15 01:04:11 -0500

    Format layout

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Syne:wght@400..800&display=swap');
+    
     </style>
     <div className="container">
       <Header />
