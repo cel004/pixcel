841a8df9d2ed88bf1dd0539e4cd3ed8bd652746a
cel004 <ngcelinaa@gmail.com>
2024-12-15 01:37:55 -0500

    Complete header file

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import styles from './Header.module.css'
+import logo from '/pixcel_logo.png'
+import Button from './Button/Button.jsx'
+
+function Header() {
+    return (
+      <>
+      <div className={styles.headerBar}>
+        <div className={styles.title}>Pixcel</div>
+          <div className={styles.right}>
+            <div className={styles.navigationBar}>
+              <a href="#">About</a>
+              <a href="#">Pricing</a>
+              <a href="#">Contact</a>
+            </div>
+            <img src={logo} alt="Pixcel's logo" />
+          </div>
+      </div>
+      <div className={styles.textContainer}>
+        <p style={{ fontWeight: '500', fontSize: '1.5rem'}}>Hand-coded with care,</p>
+        <p>our websites are crafted with empathy to redefine 
+          your digital presence, focusing on what truly matters to you.</p>
+      </div>
+      <div className={styles.detailsContainer}>
+        <p>100% Handmade</p>
+        <p>Fully Responsive</p>
+        <p>Optimized Page Speed</p>
+        <p>SEO</p>
+      </div>
+      <div className={styles.buttonContainer}>
+        <Button link="#">Get started</Button>
+      </div>
+      </>
+    )
+  }
+  
+  export default Header
+  
