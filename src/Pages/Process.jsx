d1b0f672deeac1dd37a2d17f0957f7308c175f4b
cel004 <ngcelinaa@gmail.com>
2024-12-16 01:43:57 -0500

    Enter variable data for process cards

--- src/Pages/Process.jsx
+++ src/Pages/Process.jsx
@@ -1,18 +1,49 @@
 import styles from './Process.module.css'
 import Title from '../components/Title/Title.jsx'
+import ProcessCard from '../components/ProcessCard/ProcessCard.jsx'
+
 
 function Process() {
     return (
-    <>
+      <>
       <div className={styles.container}>
         <Title>Our Process</Title>
-        <p>Based in Toronto, we blend creativity with technicality to create digital experiences.</p>
-        <p>We don’t just build websites; we build lasting relationships, understanding your business 
-            inside and out to deliver solutions that drive real results.</p>
+        <ProcessCard number="1" title="Get in touch">
+          <p>
+            An open conversation to understand your goals
+            and values to lay the foundation for success.
+          </p>
+        </ProcessCard>
+        <ProcessCard number="2" title="Vision">
+          <p>
+          Understand your needs, gather your content, 
+          and outline the layout that aligns with your business goals.
+          </p>
+        </ProcessCard>
+        <ProcessCard number="3" title="Design">
+          <p>
+          A curated mock-up design will be shared for your review and feedback.
+          </p>
+        </ProcessCard>
+        <ProcessCard number="4" title="Build">
+          <p>
+          Your fully-functional website will be developed with precision and care.
+          </p>
+        </ProcessCard>
+        <ProcessCard number="5" title="Launch">
+          <p>
+          Once polished, your website will go live, delivering your brand to the world.
+          </p>
+        </ProcessCard>
+        <ProcessCard number="6" title="Support">
+          <p>
+          Final adjustments are made for speed, performance, and user experience.
+          </p>
+        </ProcessCard>
       </div>
-      </>
-    )
-  }
+    </>
+  );
+}
   
   export default Process
   
